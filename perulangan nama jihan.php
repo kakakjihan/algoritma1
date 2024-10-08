9c5189e7a90cb8f35a451ad9b9eb2c41b4fe52f1
kakakjihan <jihansahfitri6@gmail.com>
2024-10-08 08:22:48 +0700

    komit pertama

---
+++ perulangan nama jihan.php
@@ -0,0 +1,12 @@
+<?php
+//Mulai
+//Inisialisasi variabel i dengan nilai 1.
+//Lakukan perulangan selama i kurang dari atau sama dengan 10:
+//Cetak nilai i.//Tambahkan 1 pada i.
+for ($i = 1; $i <= 10; $i++) 
+//akhiri pengulangan
+{
+    echo $i." . nama saya jihan <br>";
+}
+//selesai
+?>
